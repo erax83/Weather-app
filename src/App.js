d2b287b8d45ce8475e8737cf34b1145e82519cce
Erik Axelsson <e.axelsson@tutanota.com>
2020-04-13 19:08:24 +0200

    Add geolocation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 
+
 /*
   const weatherApi = {
   apiUrl: 'https://api.openweathermap.org/data/2.5/',
@@ -9,42 +10,91 @@ import "./App.css";
   */
 
 export default class App extends React.Component {
+
+  getCoordinates() {
+    console.log('get cordinates');
+
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(this.getCoordinatesTwo);
+    }else {
+      console.log('bla bla error');
+    }
+    
+    /*
+     navigator.geolocation.getCurrentPosition(function(position){
+      console.log(position.coords.latitude);
+      console.log(position.coords.longitude);
+    });
+    */
+    
+    
+  }
+
+  getCoordinatesTwo(position) {
+    this.setState({
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude
+    });
+    console.log(this.state.latitude);
+    this.getWeather();
+  }
+
+
   getWeather = async () => {
+    console.log('get weather');
     const api_call = await fetch(
-      "https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=8ab41d8883881d150eed8cb9119dcb3b"
+      `https://api.openweathermap.org/data/2.5/weather?lat=${this.state.latitude}&lon=${this.state.longitude}&appid=8ab41d8883881d150eed8cb9119dcb3b`
     );
     const response = await api_call.json();
+
+
     this.setState({
+      city: response.name,
       temperature: response.main.temp,
       windSpeed: response.wind.speed,
       humidity: response.main.humidity,
       sunrise: response.sys.sunrise,
       sunset: response.sys.sunset,
     });
+
+    
   };
 
   constructor(props) {
     super(props);
     this.state = {
-      city: 'London',
+      city: '',
       temperature: '',
       windSpeed: '',
       humidity: '',
       sunrise: '',
       sunset: '',
+      latitude: null,
+      longitude: null,
     };
-    this.getWeather();
+    this.getCoordinates = this.getCoordinates.bind(this);
+    this.getCoordinatesTwo = this.getCoordinatesTwo.bind(this);
+    
+    this.getCoordinates();
+    //this.getWeather();
+    
+    
   }
 
   componentDidMount() {
-    console.log("hello");
-  }
+    console.log("did mount");
+    
+    //this.getCoordinates();
+    //this.getWeather();
 
+    }
+    
   render() {
     return (
       <div>
         <header>
           <h1>Weather Report</h1>
+          
         </header>
 
         <main>
@@ -65,3 +115,4 @@ export default class App extends React.Component {
     );
   }
 }
+
